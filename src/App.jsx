978b2f0bfb32bda2beeae36d057ba08ed1a6b644
MdAbdullahAnwar <abdullahanwar2109@gmail.com>
2025-04-02 01:13:52 +0530

    Fetch Movie on Page load using useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import MoviesList from './components/MoviesList';
 import './App.css';
 
@@ -8,13 +8,14 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  async function fetchMoviesHandler() {
+
+  const fetchMoviesHandler = useCallback(async () => {
 
     setIsLoading(true);
     setError(null);
 
     try {
-      const response = await fetch('https://swapi.dev/api/films');
+      const response = await fetch('https://swapi.dev/api/films/');
       
       if(!response.ok) {
         throw new Error('Something went wrong!');
@@ -31,14 +32,19 @@ function App() {
         };
       });
       
-      setMovies(data.results);
-      setIsLoading(false);
+      setMovies(transformedMovies);
     } 
     catch (error) {
       setError(error.message);
-      setIsLoading(false);
     }
-  }
+    setIsLoading(false);
+  }, [])
+
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
+
 
   let content = <p>No Movies Found!</p>;
 
