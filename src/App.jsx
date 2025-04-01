566ec7fe903c16bf59b5f23aeab2bf0ec675c953
MdAbdullahAnwar <abdullahanwar2109@gmail.com>
2025-04-01 19:11:13 +0530

    Loading the Application

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,12 @@ import './App.css';
 function App() {
 
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
 
   async function fetchMoviesHandler() {
+
+    setIsLoading(true);
 
     const response = await fetch('https://swapi.dev/api/films');
     const data = await response.json();
@@ -19,9 +23,9 @@ function App() {
         releaseData: movieData.release_date,
       };
     });
-        
-    setMovies(data.results);
     
+    setMovies(data.results);
+    setIsLoading(false);
   }
   
 
@@ -31,7 +35,9 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies}/>
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies}/>}
+        {!isLoading && movies.length === 0 && <p>No Movies Found</p>}
+        {isLoading && <p>...Loading</p>}
       </section>
     </React.Fragment>
   )
